16b86256f0f86f00262471ed895a00e68ba4ba75
Valentin Bourcier <bourcier.valentin.a@gmail.com>
2021-12-26 23:38:56 +0100

    Feature (login) : Adding the login form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'chrome-extension-lab';
+export class AppComponent implements OnInit {
+    title = "chrome-extension-lab";
+
+    signinForm = new FormGroup({
+        firstname: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2)
+        ]),
+        lastname: new FormControl("", [
+            Validators.required,
+            Validators.minLength(2)
+        ])
+    });
+
+    get ctrls() {
+        return this.signinForm.controls;
+    }
+
+    ngOnInit(): void {}
+
+    onSubmit() {
+        const firstname = this.signinForm.value.firstname;
+        const lastname = this.signinForm.value.lastname;
+    }
 }
